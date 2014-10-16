5510d49b7df6c84932138d1d836b385e9a81d9f0
qxzcode <qxzcraft@gmail.com>
2014-10-15 18:10:35 -0700

    Progress on implementing arithmetic coding

--- src/Encoder.cpp
+++ src/Encoder.cpp
@@ -10,3 +10,26 @@
 
 using namespace dgrid::util;
 
+Encoder::Encoder():low(0),high(0x7FFFFFFF),curByte(0),curBits(0) {
+	
+}
+
+void Encoder::encode(uint32_t low_count, uint32_t high_count, uint32_t total) {
+	// compute step
+	uint32_t step = (high - low + 1) / total;
+	
+	// update current interval
+	high = low + high_count*step - 1;
+	low  = low + low_count*step;
+	
+	// if possible, write bits and apply scaling
+	//...
+}
+
+void Encoder::pushBit(bool bit) {
+	curByte = (curByte*2) + bit;
+	if (++curBits == 8) {
+		bytes.push_back(curByte);
+		curByte = curBits = 0;
+	}
+}
--- src/Encoder.h
+++ src/Encoder.h
@@ -8,14 +8,27 @@
 
 #pragma once
 
+#include <stdint.h>
+#include <vector>
+
 namespace dgrid { namespace util {
+	
+	typedef unsigned char byte;
 	
 	class Encoder {
 	public:
+		Encoder();
 		
+		void encode(uint32_t low_count, uint32_t high_count, uint32_t total);
+		byte* data() { return &bytes[0]; }
+		std::vector<byte>::size_type len() { return bytes.size(); }
 		
 	protected:
+		void pushBit(bool bit);
 		
+		uint32_t low, high;
+		std::vector<byte> bytes;
+		byte curByte, curBits;
 		
 	};
 
