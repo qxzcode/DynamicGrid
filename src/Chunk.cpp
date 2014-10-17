c68e8a266c63dcb322d86416cd6f8e9125d54a0a
qxzcode <qxzcraft@gmail.com>
2014-10-17 12:11:23 -0700

    Implemented very simple RLE. Results: 128kb -> 7-9kb

--- src/Chunk.cpp
+++ src/Chunk.cpp
@@ -124,32 +124,42 @@ void Chunk::compress(unsigned char* &data, unsigned int &len) {
 	printf("Compressing chunk (%i, %i)...\n", cx, cy);
 	util::Encoder encoder;
 	
+	struct run{unsigned len,max;tileID tile;};
+	
 	// encode data
 	for (int l = 0; l < NUM_LAYERS-1; l++) {
+		// parse runs & count symbols
+		std::vector<run> runs;
 		uint32_t counts[10];
 		for (int i=0; i<10; i++) counts[i] = 0;
-		
 		for (int x = 0; x < CHUNK_SIZE; x++) {
+			run curRun;
 			for (int y = 0; y < CHUNK_SIZE; y++) {
-				counts[getTile(l, x, y)]++;
+				tileID t = getTile(l, x, y);
+				if (y==0) {
+					curRun = {0,CHUNK_SIZE,t};
+					counts[t]++;
+				} else if (t != curRun.tile) {
+					runs.push_back(curRun);
+					curRun = {0,curRun.max-curRun.len,t};
+					counts[t]++;
+				} else {
+					curRun.len++;
+				}
 			}
+			runs.push_back(curRun);
 		}
+		
+		// populate SymbolSet
 		util::SymbolSet symbols;
-		printf("Counts for layer %i:\n", l);
-		for (int i=0; i<10; i++) {
+		for (int i = 0; i < 10; i++) {
 			symbols.addSymbol(counts[i]);
-			
-			encoder.encode(counts[i], CHUNK_SIZE*CHUNK_SIZE+1);
-			encoder.encode(i, 256);
-			if (counts[i] > 0) {
-				printf("\t%i: %i (%f%%)\n", i, counts[i], 100*float(counts[i])/float(CHUNK_SIZE*CHUNK_SIZE));
-			}
 		}
 		
-		for (int x = 0; x < CHUNK_SIZE; x++) {
-			for (int y = 0; y < CHUNK_SIZE; y++) {
-				encoder.encode(symbols, getTile(l, x, y));
-			}
+		// encode runs
+		for (run r : runs) {
+			encoder.encode(r.len, r.max);
+			encoder.encode(symbols, r.tile);
 		}
 	}
 	encoder.finish();
@@ -158,6 +168,8 @@ void Chunk::compress(unsigned char* &data, unsigned int &len) {
 	printf("Bytes before compression: %lu (%lukb)\n", before, before/1024);
 	printf("Bytes after compression: %lu (%lukb)\n", after, after/1024);
 	printf("Compression ratio: %f%%\n\n\n", 100*float(after)/float(before));
+	
+	return;
 	
 	// check data
 	util::Decoder decoder(encoder.data(), encoder.len());
